c9c3dace670f3e95e469a373f89daeb3d4ac637a
Brendan <brendan.oshea@hotmail.co.uk>
2023-07-06 19:46:57 +0100

    branch to make it compatible with firestore

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,8 @@
 import { useStoreActions, useStoreState } from "easy-peasy";
-import { useState } from "react";
-import { addData } from "../requests";
-
+import {   useState } from "react";
+import { addData, handleDel} from "../requests";
+import { collection, getDocs } from "firebase/firestore"; 
+import { db } from "../firebase";
 const Blogs = () => {
 // const [blogs, setBlogs] = useState([])
 const [blog,setBlog] = useState('')
@@ -12,8 +13,9 @@ const blogs = useStoreState((state)=>state.blogs)
 const setBlogs = useStoreActions((actions) => actions.setBlogs)
 
     const handleSubmit = (post, title, id) => {
+        const date = new Date().toLocaleString()
         if (post && title){
-            setBlogs([...blogs, {title, post , date: new Date().toLocaleString(), id}])
+            // setBlogs([...blogs, {title, post , date: new Date().toLocaleString(), id}])
             setBlog('')
             setTitle('')
         } else {
@@ -27,20 +29,44 @@ const setBlogs = useStoreActions((actions) => actions.setBlogs)
                 alert('Enter in a post and a title')
             }
         }
-        addData()
+        addData(post, title, id, date)
     }
 
     const handleDelete = (id) => {
         const filtered = blogs.filter((post)=>post.id !== id)
-        setBlogs([...filtered])
+        // setBlogs([...filtered])
+        handleDel()
     }
     
-    const handleEdit = () => {
+    const handleEdit = (e) => {
+        console.log(e)
         const mapped = blogs.map((item)=>item.id === edit.id? edit : item)
         setBlogs(mapped)
         setEdit({id:null,title:'',post:''})
     }
 
+    const handleget = async() => {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        const querySnapshot1 = await querySnapshot
+        const x = []
+            for (var i in querySnapshot.docs){
+        const id = querySnapshot1.docs[i]._document.data.value.mapValue.fields.id.integerValue
+        const post = querySnapshot1.docs[i]._document.data.value.mapValue.fields.post.stringValue
+        const title = querySnapshot1.docs[i]._document.data.value.mapValue.fields.title.stringValue
+        const date = querySnapshot1.docs[i]._document.data.value.mapValue.fields.date.stringValue
+        x.push({title,post,id,date})
+    }
+
+    return x
+    }
+
+    handleget().then(function(result){
+        setBlogs(result)
+    })
+
+    
+  
+       
 
     if (edit.id) {
         return (
@@ -52,7 +78,7 @@ const setBlogs = useStoreActions((actions) => actions.setBlogs)
          <textarea  className='border-black border-2 m-4 rounded p-3' name="textarea" id="textarea" cols="90" rows="10"
                     value={edit.post} onChange={(e)=>setEdit({...edit,post: e.target.value})}></textarea>
          <button className=" border-2 border-black px-2 py-1 rounded object-center"
-         onClick={()=>handleEdit()}>Submit</button>
+         onClick={(e)=>handleEdit(e)}>Submit</button>
          </>
         )
     }
@@ -75,7 +101,9 @@ const setBlogs = useStoreActions((actions) => actions.setBlogs)
      
         <div>
             <h1 className="text-2xl mx-8 mt-6">Blog Post</h1>
-            {blogs.slice().reverse().map((blogg)=>{
+            {blogs
+            // .slice().reverse()
+            .map((blogg)=>{
                return(
                    <div key={blogg.id} className="border-black border-2 my-10 mx-4 rounded">
                     <p className='mx-8 my-3 text-2xl'>{blogg.title}</p>
@@ -84,7 +112,8 @@ const setBlogs = useStoreActions((actions) => actions.setBlogs)
                   {loggedIn ? <div><button className="border-black border-2 rounded px-2 float-right top-0"
                            onClick={()=>{handleDelete(blogg.id)}}>Delete</button>
                    <button className="border-black border-2 rounded px-2 float-right top-0"
-                            onClick={()=> setEdit({id:blogg.id, title:blogg.title, post: blogg.post,date:blogg.date})}>Edit</button></div> : ''}
+                            onClick={(e)=> 
+                                    setEdit({id:blogg.id, title:blogg.title, post: blogg.post,date:blogg.date})}>Edit</button></div> : ''}
                 </div>
                    
                    )
